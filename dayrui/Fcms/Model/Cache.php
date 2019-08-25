7b92a2e2db831a7bbe578d687a62490d3a9e74f2
PHP码农 <q@xunruicms.com>
2019-08-25 09:39:48 +0800

    引导提示

--- dayrui/Fcms/Model/Cache.php
+++ dayrui/Fcms/Model/Cache.php
@@ -36,9 +36,9 @@ class Cache extends \Phpcmf\Model
         $page = intval($_GET['page']);
         if (!$page) {
             if (!SYS_CACHE) {
-                exit(\Phpcmf\Service::C()->_json(0, dr_lang('系统未开启缓存功能'), 1));
+                exit(\Phpcmf\Service::C()->_json(0, '<a href="'.dr_url('system_cache/index').'">'.dr_lang('系统未开启缓存功能').'</a>', 1));
             } elseif (!SYS_CACHE_ATTACH) {
-                exit(\Phpcmf\Service::C()->_json(0, dr_lang('系统未设置附件缓存时间'), 1));
+                exit(\Phpcmf\Service::C()->_json(0, '<a href="'.dr_url('system_cache/index').'">'.dr_lang('系统未设置附件缓存时间').'</a>', 1));
             }
             exit(\Phpcmf\Service::C()->_json(1, dr_lang('正在检查附件'), 1));
         }
