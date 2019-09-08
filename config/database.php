c74e6cde53e6ab2e5b6119b718895388e8d5d15d
迅睿云软件 <dayrui@gmail.com>
2019-09-08 10:33:54 +0800

    fix

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * 数据库配置文件
+ */
+
+$db['default']	= [
+    'hostname'	=> 'localhost',
+    'username'	=> '',
+    'password'	=> '',
+    'database'	=> '',
+    'DBPrefix'	=> 'dr_',
+];
