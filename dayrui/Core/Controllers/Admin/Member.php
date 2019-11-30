d7b752847f30682ccdb55196a448eca90691c236
PHP码农 <q@xunruicms.com>
2019-11-30 11:23:26 +0800

    fix 去除多余代码

--- dayrui/Core/Controllers/Admin/Member.php
+++ dayrui/Core/Controllers/Admin/Member.php
@@ -507,25 +507,7 @@ class Member extends \Phpcmf\Table
      * 后台授权登录
      */
     public function alogin_index() {
-
-        $uid = intval(\Phpcmf\Service::L('input')->get('id'));
-        $code = md5($this->admin['id'].$this->admin['password']);
-        
-        \Phpcmf\Service::L('cache')->set_data('admin_login_member', $this->admin, 300);
-
-        $sso = '';
-        $url = \Phpcmf\Service::M('member')->get_sso_url();
-        foreach ($url as $u) {
-            $sso.= '<script src="'.$u.'index.php?s=api&c=sso&action=alogin&code='.dr_authcode($uid.'-'.$code, 'ENCODE').'"></script>';
-        }
-        \Phpcmf\Service::V()->assign([
-            'menu' => '',
-        ]);
-
-        $url = urldecode(\Phpcmf\Service::L('input')->get('url', true));
-        !$url && $url = MEMBER_URL;
-
-        $this->_msg(1, dr_lang('正在授权登录此用户...').$sso, $url, 0);exit;
+        $this->_msg(1, dr_lang('正在授权登录此用户...'), dr_url('api/alogin', ['id' => intval(\Phpcmf\Service::L('input')->get('id'))]), 0);exit;
     }
 
     /**
