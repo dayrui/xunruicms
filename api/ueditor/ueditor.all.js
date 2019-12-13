b57d85e3585181bd7e19cd20edf7d3bfec6427ca
PHP码农 <dayrui@gmail.com>
2019-12-13 21:23:15 +0800

    fix bubaochuntis

--- api/ueditor/ueditor.all.js
+++ api/ueditor/ueditor.all.js
@@ -29548,18 +29548,7 @@ UE.registerUI('message', function(editor) {
 UE.registerUI('autosave', function(editor) {
     var timer = null,uid = null;
     editor.on('afterautosave',function(){
-        clearTimeout(timer);
-
-        timer = setTimeout(function(){
-            if(uid){
-                editor.trigger('hidemessage',uid);
-            }
-            uid = editor.trigger('showmessage',{
-                content : editor.getLang('autosave.success'),
-                timeout : 2000
-            });
-
-        },2000)
+       
     })
 
 });
