d8f2cacdd3c82a037ef122d4545f9c9845a06b3b
PHP码农 <q@xunruicms.com>
2020-05-13 16:29:25 +0800

    优化数据库编码为utf8mb4

--- dayrui/Fcms/Core/Helper.php
+++ dayrui/Fcms/Core/Helper.php
@@ -328,7 +328,11 @@ if (!function_exists('dr_domain_301')) {
 
 // 格式化sql创建
 function dr_format_create_sql($sql) {
-    return trim(str_replace('ENGINE=MyISAM', 'ENGINE=InnoDB', $sql));
+    $sql = trim(str_replace('ENGINE=MyISAM', 'ENGINE=InnoDB', $sql));
+    if (version_compare(\Phpcmf\Service::M()->db->getVersion(), '5.6.0') > 0) {
+        $sql = trim(str_replace('CHARSET=utf8', 'CHARSET=utf8mb4', $sql));
+    }
+    return $sql;
 }
 
 /**
