a51a4291c903713444ca4d434af4a2d45621775c
PHP码农 <q@xunruicms.com>
2020-05-15 10:46:01 +0800

    应用安装时的检测支撑文件

--- dayrui/Core/Controllers/Admin/Cloud.php
+++ dayrui/Core/Controllers/Admin/Cloud.php
@@ -263,27 +263,25 @@ class Cloud extends \Phpcmf\Common
     function install_app() {
 
         $id = dr_safe_replace($_GET['id']);
+        $cmspath = WRITEPATH.'temp/'.$id.'/';
 		if (!IS_DEV) {
 			$cache = \Phpcmf\Service::L('cache')->get_data('cloud-update-'.$id);
 			if (!$cache) {
 				$this->_json(0, '本站：授权验证缓存过期，请重试');
 			}
+            $file = WRITEPATH.'temp/'.$id.'.zip';
+            if (!is_file($file)) {
+                $this->_json(0, '本站：文件还没有被下载');
+            } elseif (!class_exists('ZipArchive')) {
+                $this->_json(0, '本站：php_zip扩展未开启，无法在线安装功能');
+            }
+
+            // 解压目录
+            if (!\Phpcmf\Service::L('file')->unzip($file, $cmspath)) {
+                $this->_json(0, '本站：文件解压失败');
+            }
+            unlink($file);
 		}
-
-        $file = WRITEPATH.'temp/'.$id.'.zip';
-        if (!is_file($file)) {
-            $this->_json(0, '本站：文件还没有被下载');
-        } elseif (!class_exists('ZipArchive')) {
-            $this->_json(0, '本站：php_zip扩展未开启，无法在线安装功能');
-        }
-
-        // 解压目录
-        $cmspath = WRITEPATH.'temp/'.$id.'/';
-        if (!\Phpcmf\Service::L('file')->unzip($file, $cmspath)) {
-            $this->_json(0, '本站：文件解压失败');
-        }
-
-        unlink($file);
 
 		// 查询插件目录
         $is_app = $is_module_app = $is_tpl = 0;
