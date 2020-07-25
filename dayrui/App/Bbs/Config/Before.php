630533b47947315e7f2230842dc46104c53a2fa4
PHP码农 <q@xunruicms.com>
2020-07-25 10:02:38 +0800

    bbs模块安装时自动创建评论配置程序

--- dayrui/App/Bbs/Config/Before.php
+++ dayrui/App/Bbs/Config/Before.php
@@ -1,6 +1,5 @@
 <?php
 
-
 if (!is_dir(dr_get_app_dir('comment'))) {
 	return dr_return_data(0, '请下载官方版的评论系统插件');
 }
@@ -12,30 +11,6 @@ if (!is_file(dr_get_app_dir('comment').'install.lock')) {
 	}
 }
 
-$config = [
-	'use' => 1,
-	'cname' => '帖子',
-	'ct_reply' => 1,
-];
-
-$ct = \Phpcmf\Service::M()->table('app_comment')->where('name', 'module')->getRow();
-if ($ct) {
-	$ct_cfg = dr_string2array($ct['value']);
-	if (!$ct_cfg[$dir]) {
-		\Phpcmf\Service::M()->db->table('app_comment')->where('name', 'module')->update([
-			'value' => dr_array2string([
-				$dir => $config,
-			]),
-		]);
-	}
-} else {
-	\Phpcmf\Service::M()->db->table('app_comment')->insert([
-		'name' => 'module',
-		'value' => dr_array2string([
-			$dir => $config,
-		]),
-	]);
-}
 
 
 return dr_return_data(1, 'ok');
