456be514efd27b04707424a0a2a96b309253d30d
PHP码农 <q@xunruicms.com>
2020-08-15 07:08:39 +0800

    version 4.3.10

--- dayrui/Fcms/Core/Phpcmf.php
+++ dayrui/Fcms/Core/Phpcmf.php
@@ -1080,7 +1080,7 @@ abstract class Common extends \CodeIgniter\Controller
 
     // 版本检查
     protected function _api_version_cmf() {
-        $json = dr_catcher_data('https://www.xunruicms.com/version.php?action=new&id=1&v='.$this->cmf_version['version']);
+        $json = dr_catcher_data('https://www.xunruicms.com/version.php?action=new&id=1&time='.strtotime($this->cmf_version['downtime']).'&v='.$this->cmf_version['version']);
         exit($json);
     }
 
