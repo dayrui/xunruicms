db9c2cfc6760a7a1f62a6bcbb3eeb33ac85fa279
PHP码农 <dayrui@gmail.com>
2020-08-23 09:52:33 +0800

    cli

--- dayrui/Fcms/Init.php
+++ dayrui/Fcms/Init.php
@@ -64,26 +64,16 @@ if (is_file(WRITEPATH.'config/system.php')) {
 foreach ($system as $var => $value) {
     !defined($var) && define($var, $value);
 }
-
-// 当前URL
-$pageURL = 'http';
-((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
-    || (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')
-    || (defined('IS_HTTPS_FIX') && IS_HTTPS_FIX)
-    || (!IS_ADMIN && isset($system['SYS_HTTPS']) && $system['SYS_HTTPS'])) && $pageURL.= 's';
-$pageURL.= '://';
-// 优先定义后台域名
-IS_ADMIN && define('ADMIN_URL', $pageURL.$_SERVER['HTTP_HOST'].'/');
-if (strpos($_SERVER['HTTP_HOST'], ':') !== FALSE) {
-    $url = explode(':', $_SERVER['HTTP_HOST']);
-    $url[0] ? $pageURL.= $_SERVER['HTTP_HOST'] : $pageURL.= $url[0];
-} else {
-    $pageURL.= $_SERVER['HTTP_HOST'];
-}
-
-define('FC_NOW_URL', $pageURL.($_SERVER['REQUEST_URI'] ? $_SERVER['REQUEST_URI'] : $_SERVER['PHP_SELF']));
-define('FC_NOW_HOST', $pageURL.'/');
 unset($system);
+
+// 显示错误提示
+if (CI_DEBUG) {
+    ini_set('display_errors', 1);
+    // 重置Zend OPcache
+    function_exists('opcache_reset') && opcache_reset();
+} else {
+    ini_set('display_errors', 0);
+}
 
 // 缓存变量
 $cache = [];
@@ -104,79 +94,6 @@ foreach ([
 }
 unset($cache);
 
-// 当前域名
-define('DOMAIN_NAME', strtolower($_SERVER['HTTP_HOST']));
-
-// 伪静态字符串
-$uu = isset($_SERVER['HTTP_X_REWRITE_URL']) || trim($_SERVER['REQUEST_URI'], '/') == SELF ? trim($_SERVER['HTTP_X_REWRITE_URL'], '/') : ($_SERVER['REQUEST_URI'] ? trim($_SERVER['REQUEST_URI'], '/') : NULL);
-
-// 以index.php或者?开头的uri不做处理
-$uri = strpos($uu, SELF) === 0 || strpos($uu, '?') === 0 ? '' : $uu;
-
-// 当前URI
-define('CMSURI', $uri);
-
-// 根据自定义URL规则来识别路由
-if (!IS_ADMIN && $uri && !defined('IS_API')) {
-    // 自定义URL解析规则
-    $routes = [];
-    $routes['rewrite-test.html(.*)'] = 'index.php?s=api&c=rewrite&m=test'; // 测试规则
-    if (is_file(ROOTPATH.'config/rewrite.php')) {
-        $my = require ROOTPATH.'config/rewrite.php';
-        $my && $routes = array_merge($routes, $my);
-    }
-    // 正则匹配路由规则
-    $is_404 = 1;
-    foreach ($routes as $key => $val) {
-        $rewrite = $match = [];
-        if ($key == $uri || preg_match('/^'.$key.'$/U', $uri, $match)) {
-            unset($match[0]);
-            // 开始匹配
-            $is_404 = 0;
-            // 开始解析路由 URL参数模式
-            $_GET = [];
-            $queryParts = explode('&', str_replace('index.php?', '', $val));
-            if ($queryParts) {
-                foreach ($queryParts as $param) {
-                    $item = explode('=', $param);
-                    $_GET[$item[0]] = $item[1];
-                    if (strpos($item[1], '$') !== FALSE) {
-                        $id = (int)substr($item[1], 1);
-                        $_GET[$item[0]] = isset($match[$id]) ? $match[$id] : $item[1];
-                    }
-                }
-            }
-            !$_GET['c'] && $_GET['c'] = 'home';
-            !$_GET['m'] && $_GET['m'] = 'index';
-            // 结束匹配
-            break;
-        }
-    }
-    // 自定义路由模式
-    if ($is_404 && is_file(ROOTPATH.'config/router.php')) {
-        require ROOTPATH.'config/router.php';
-    }
-    // 说明是404
-    if ($is_404) {
-        $_GET['s'] = '';
-        $_GET['c'] = 'home';
-        $_GET['m'] = 's404';
-        $_GET['uri'] = $uri;
-    }
-}
-
-// API接口项目标识 放到后面是为了识别api 的伪静态
-!defined('IS_API') && define('IS_API', isset($_GET['s']) && $_GET['s'] == 'api');
-
-// 解析自定义域名
-if (!IS_API && $_GET['s'] != 'api' && is_file(WRITEPATH.'config/domain_app.php')){
-    $domain = require WRITEPATH.'config/domain_app.php';
-    // 强制定义为模块
-    if (isset($domain[DOMAIN_NAME]) && $domain[DOMAIN_NAME] && is_dir(APPSPATH.ucfirst($domain[DOMAIN_NAME]))) {
-        $_GET['s'] = $domain[DOMAIN_NAME];
-    }
-    unset($domain);
-}
 
 // 自定义开发目录分布
 if (is_file(MYPATH.'Dev.php')) {
@@ -216,47 +133,6 @@ function dr_show_error($msg) {
     exit("<!DOCTYPE html><html lang=\"zh-cn\"><head><meta charset=\"utf-8\"><title>系统错误</title><style>        div.logo {            height: 200px;            width: 155px;            display: inline-block;            opacity: 0.08;            position: absolute;            top: 2rem;            left: 50%;            margin-left: -73px;        }        body {            height: 100%;            background: #fafafa;            font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif;            color: #777;            font-weight: 300;        }        h1 {            font-weight: lighter;            letter-spacing: 0.8;            font-size: 3rem;            margin-top: 0;            margin-bottom: 0;            color: #222;        }        .wrap {            max-width: 1024px;            margin: 5rem auto;            padding: 2rem;            background: #fff;            text-align: center;            border: 1px solid #efefef;            border-radius: 0.5rem;            position: relative;        }        pre {            white-space: normal;            margin-top: 1.5rem;        }        code {            background: #fafafa;            border: 1px solid #efefef;            padding: 0.5rem 1rem;            border-radius: 5px;            display: block;        }        p {            margin-top: 1.5rem;        }        .footer {            margin-top: 2rem;            border-top: 1px solid #efefef;            padding: 1em 2em 0 2em;            font-size: 85%;            color: #999;        }        a:active,        a:link,        a:visited {            color: #dd4814;        }</style></head><body><div class=\"wrap\"><p>{$msg}</p>    {$url}</div></body></html>");
 }
 
-// 判断s参数,“应用程序”文件夹目录
-if (!IS_API && isset($_GET['s']) && preg_match('/^[a-z_]+$/i', $_GET['s'])) {
-    // 判断会员模块,排除后台调用
-    $dir = ucfirst($_GET['s']);
-    if (!IS_ADMIN && $dir == 'Member') {
-        // 会员
-        if ($_GET['app'] && dr_is_app_dir($_GET['app'])) {
-            // 模块应用
-            define('APPPATH', dr_get_app_dir($_GET['app']));
-            define('APP_DIR', strtolower($_GET['app'])); // 应用目录名称
-        } else {
-            // 表示会员模块
-            define('APPPATH', COREPATH);
-            define('APP_DIR', ''); // 模块目录名称
-        }
-        define('IS_MEMBER', TRUE);
-    } elseif (dr_is_app_dir($dir)) {
-        // 模块应用
-        define('APPPATH', dr_get_app_dir($dir));
-        define('APP_DIR', strtolower($dir)); // 应用目录名称
-        define('IS_MEMBER', FALSE);
-    } else {
-        // 不存在的应用
-        dr_show_error(CI_DEBUG ? '应用程序('.dr_get_app_dir($dir).')不存在' : '应用程序('.strtolower($dir).')不存在');
-    }
-} else {
-    // 系统主目录
-    !defined('APPPATH') && define('APPPATH', COREPATH);
-    !defined('APP_DIR') && define('APP_DIR', '');
-    define('IS_MEMBER', FALSE);
-}
-
-// 显示错误提示
-if (CI_DEBUG) {
-    ini_set('display_errors', 1);
-    // 重置Zend OPcache
-    function_exists('opcache_reset') && opcache_reset();
-} else {
-    ini_set('display_errors', 0);
-}
-
 /*
  * 重写config函数，防止modules被加载
  */
@@ -284,6 +160,150 @@ if (!function_exists('locale_set_default')) {
 function log_message($level, $message, array $context = []) {
     return Phpcmf\Service::Log($level, $message, $context);
 }
+
+
+if (PHP_SAPI === 'cli' || defined('STDIN')) {
+	// CLI命令行模式
+	 define('ADMIN_URL', 'http://localhost/');
+	 define('FC_NOW_URL', 'http://localhost/');
+	 define('FC_NOW_HOST', 'http://localhost/');
+	 define('DOMAIN_NAME', 'http://localhost/');
+	if ($_SERVER["argv"]) {
+		foreach ($_SERVER["argv"] as $val) {
+			if (strpos($val, '=') !== false) {
+				list($name) = explode('=', $val);
+				$_GET[$name] = substr($val, strlen($name)+1);
+			}
+		}
+	}
+	 define('ENVIRONMENT', 'testing');
+} else {
+	// 正常访问模式
+	// 当前URL
+	$pageURL = 'http';
+	((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
+		|| (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443')
+		|| (defined('IS_HTTPS_FIX') && IS_HTTPS_FIX)
+		|| (!IS_ADMIN && isset($system['SYS_HTTPS']) && $system['SYS_HTTPS'])) && $pageURL.= 's';
+	$pageURL.= '://';
+	// 优先定义后台域名
+	IS_ADMIN && define('ADMIN_URL', $pageURL.$_SERVER['HTTP_HOST'].'/');
+	if (strpos($_SERVER['HTTP_HOST'], ':') !== FALSE) {
+		$url = explode(':', $_SERVER['HTTP_HOST']);
+		$url[0] ? $pageURL.= $_SERVER['HTTP_HOST'] : $pageURL.= $url[0];
+	} else {
+		$pageURL.= $_SERVER['HTTP_HOST'];
+	}
+	define('FC_NOW_URL', $pageURL.($_SERVER['REQUEST_URI'] ? $_SERVER['REQUEST_URI'] : $_SERVER['PHP_SELF']));
+	define('FC_NOW_HOST', $pageURL.'/');
+
+	// 当前域名
+	define('DOMAIN_NAME', strtolower($_SERVER['HTTP_HOST']));
+	
+	// 伪静态字符串
+	$uu = isset($_SERVER['HTTP_X_REWRITE_URL']) || trim($_SERVER['REQUEST_URI'], '/') == SELF ? trim($_SERVER['HTTP_X_REWRITE_URL'], '/') : ($_SERVER['REQUEST_URI'] ? trim($_SERVER['REQUEST_URI'], '/') : NULL);
+
+	// 以index.php或者?开头的uri不做处理
+	$uri = strpos($uu, SELF) === 0 || strpos($uu, '?') === 0 ? '' : $uu;
+
+	// 当前URI
+	define('CMSURI', $uri);
+
+	// 根据自定义URL规则来识别路由
+	if (!IS_ADMIN && $uri && !defined('IS_API')) {
+		// 自定义URL解析规则
+		$routes = [];
+		$routes['rewrite-test.html(.*)'] = 'index.php?s=api&c=rewrite&m=test'; // 测试规则
+		if (is_file(ROOTPATH.'config/rewrite.php')) {
+			$my = require ROOTPATH.'config/rewrite.php';
+			$my && $routes = array_merge($routes, $my);
+		}
+		// 正则匹配路由规则
+		$is_404 = 1;
+		foreach ($routes as $key => $val) {
+			$rewrite = $match = [];
+			if ($key == $uri || preg_match('/^'.$key.'$/U', $uri, $match)) {
+				unset($match[0]);
+				// 开始匹配
+				$is_404 = 0;
+				// 开始解析路由 URL参数模式
+				$_GET = [];
+				$queryParts = explode('&', str_replace('index.php?', '', $val));
+				if ($queryParts) {
+					foreach ($queryParts as $param) {
+						$item = explode('=', $param);
+						$_GET[$item[0]] = $item[1];
+						if (strpos($item[1], '$') !== FALSE) {
+							$id = (int)substr($item[1], 1);
+							$_GET[$item[0]] = isset($match[$id]) ? $match[$id] : $item[1];
+						}
+					}
+				}
+				!$_GET['c'] && $_GET['c'] = 'home';
+				!$_GET['m'] && $_GET['m'] = 'index';
+				// 结束匹配
+				break;
+			}
+		}
+		// 自定义路由模式
+		if ($is_404 && is_file(ROOTPATH.'config/router.php')) {
+			require ROOTPATH.'config/router.php';
+		}
+		// 说明是404
+		if ($is_404) {
+			$_GET['s'] = '';
+			$_GET['c'] = 'home';
+			$_GET['m'] = 's404';
+			$_GET['uri'] = $uri;
+		}
+	}
+
+}
+
+// API接口项目标识 放到后面是为了识别api 的伪静态
+	!defined('IS_API') && define('IS_API', isset($_GET['s']) && $_GET['s'] == 'api');
+
+	// 解析自定义域名
+	if (!IS_API && $_GET['s'] != 'api' && is_file(WRITEPATH.'config/domain_app.php')){
+		$domain = require WRITEPATH.'config/domain_app.php';
+		// 强制定义为模块
+		if (isset($domain[DOMAIN_NAME]) && $domain[DOMAIN_NAME] && is_dir(APPSPATH.ucfirst($domain[DOMAIN_NAME]))) {
+			$_GET['s'] = $domain[DOMAIN_NAME];
+		}
+		unset($domain);
+	}
+		
+	// 判断s参数,“应用程序”文件夹目录
+	if (!IS_API && isset($_GET['s']) && preg_match('/^[a-z_]+$/i', $_GET['s'])) {
+		// 判断会员模块,排除后台调用
+		$dir = ucfirst($_GET['s']);
+		if (!IS_ADMIN && $dir == 'Member') {
+			// 会员
+			if ($_GET['app'] && dr_is_app_dir($_GET['app'])) {
+				// 模块应用
+				define('APPPATH', dr_get_app_dir($_GET['app']));
+				define('APP_DIR', strtolower($_GET['app'])); // 应用目录名称
+			} else {
+				// 表示会员模块
+				define('APPPATH', COREPATH);
+				define('APP_DIR', ''); // 模块目录名称
+			}
+			define('IS_MEMBER', TRUE);
+		} elseif (dr_is_app_dir($dir)) {
+			// 模块应用
+			define('APPPATH', dr_get_app_dir($dir));
+			define('APP_DIR', strtolower($dir)); // 应用目录名称
+			define('IS_MEMBER', FALSE);
+		} else {
+			// 不存在的应用
+			dr_show_error(CI_DEBUG ? '应用程序('.dr_get_app_dir($dir).')不存在' : '应用程序('.strtolower($dir).')不存在');
+		}
+	} else {
+		// 系统主目录
+		!defined('APPPATH') && define('APPPATH', COREPATH);
+		!defined('APP_DIR') && define('APP_DIR', '');
+		define('IS_MEMBER', FALSE);
+	}
 
 
 /******* CodeIgniter Bootstrap *******/
