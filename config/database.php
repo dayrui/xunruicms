d6824a033e0f62be529d5e8a7ea128e29429a52e
PHP码农 <dayrui@gmail.com>
2020-10-05 17:38:35 +0800

    update config/database.php.

--- config/database.php
+++ config/database.php
@@ -5,9 +5,9 @@
  */
 
 $db['default']	= [
-    'hostname'	=> 'localhost',
-    'username'	=> '',
-    'password'	=> '',
-    'database'	=> '',
+    'hostname'	=> '填写mysql服务器地址，默认为localhost',
+    'username'	=> '填写数据库账号',
+    'password'	=> '填写数据库密码',
+    'database'	=> '填写数据库名称，如果不存在请手动创建',
     'DBPrefix'	=> 'dr_',
 ];
