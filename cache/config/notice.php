dd35866d56454026d75219e27d6e3a92b58ddd09
PHP码农 <q@xunruicms.com>
2020-11-19 11:48:38 +0800

    cache

---
+++ cache/config/notice.php
@@ -0,0 +1,34 @@
+<?php
+
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * 设置自定义消息类型
+ */
+
+return array (
+    1 => array (
+        'name' => '系统',
+        'icon' => 'fa fa-bell-o',
+    ),
+    2 => array (
+        'name' => '用户',
+        'icon' => 'fa fa-user',
+    ),
+    3 => array (
+        'name' => '内容',
+        'icon' => 'fa fa-th-large',
+    ),
+    4 => array (
+        'name' => '应用',
+        'icon' => 'fa fa-puzzle-piece',
+    ),
+    5 => array (
+        'name' => '交易',
+        'icon' => 'fa fa-rmb',
+    ),
+    6 => array (
+        'name' => '订单',
+        'icon' => 'fa fa-shopping-cart',
+    ),
+);
