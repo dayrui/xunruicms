5e89bdc966b3bb089a767791da4c5c697ac9ba87
PHP码农 <dayrui@gmail.com>
2021-02-12 16:30:27 +0800

    version 14

--- api/ueditor/php/tool.php
+++ api/ueditor/php/tool.php
@@ -14,7 +14,7 @@ return '
   ,"touppercase","tolowercase","|"
   ,"link","unlink","anchor","|"
   ,"imagenone","imageleft","imageright","imagecenter", "|"
-  ,"simpleupload","insertimage","emotion","scrawl","insertvideo","attachment","map","insertframe","insertcode","template","background","|"
+  ,"simpleupload","insertimage","emotion","scrawl","insertvideo","attachment","map","insertframe","insertcode","template","|"
   ,"horizontal","date","time","spechars","|"
   ,"inserttable","deletetable","insertparagraphbeforetable","insertrow","deleterow","insertcol","deletecol","mergecells","mergeright","mergedown","splittocells","splittorows","splittocols","charts","|"
   ,"print","preview","searchreplace","drafts"
