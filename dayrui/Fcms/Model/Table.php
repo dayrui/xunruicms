a2ab79e3467aaa7524aa1e22d0e4ccf455d1b290
PHP码农 <dayrui@gmail.com>
2021-03-24 19:11:00 +0800

    执行应用插件的站点sql语句

--- dayrui/Fcms/Model/Table.php
+++ dayrui/Fcms/Model/Table.php
@@ -215,5 +215,72 @@ class Table extends \Phpcmf\Model
         }
     }
 
+    // 站点--------------------------------------------------------------------
+
+    // 创建站点
+    public function create_site($siteid) {
+
+        // 创建数据表
+        if ($siteid > 1) {
+            // 复制站点1的栏目结构
+            $sql = $this->db->query("SHOW CREATE TABLE `".$this->dbprefix('1_share_category')."`")->getRowArray();
+            $sql = str_replace(
+                array($sql['Table'], 'CREATE TABLE'),
+                array('{tablename}', 'CREATE TABLE IF NOT EXISTS'),
+                $sql['Create Table']
+            );
+            $this->db->simpleQuery(str_replace('{tablename}', $this->dbprefix($siteid.'_share_category'), dr_format_create_sql($sql)));
+            $this->db->table($siteid.'_share_category')->truncate();
+        } else {
+            $this->db->simpleQuery("DROP TABLE IF EXISTS `".$this->dbprefix($siteid.'_share_category')."`");
+            $this->db->simpleQuery(dr_format_create_sql("
+        CREATE TABLE IF NOT EXISTS `".$this->dbprefix($siteid.'_share_category')."` (
+          `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+          `tid` tinyint(1) NOT NULL COMMENT '栏目类型，0单页，1模块，2外链',
+          `pid` smallint(5) unsigned NOT NULL DEFAULT '0' COMMENT '上级id',
+          `mid` varchar(20) NOT NULL COMMENT '模块目录',
+          `pids` varchar(255) NOT NULL COMMENT '所有上级id',
+          `name` varchar(255) NOT NULL COMMENT '栏目名称',
+          `dirname` varchar(255) NOT NULL COMMENT '栏目目录',
+          `pdirname` varchar(255) NOT NULL COMMENT '上级目录',
+          `child` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否有下级',
+          `childids` text NOT NULL COMMENT '下级所有id',
+          `domain` varchar(50) DEFAULT NULL COMMENT '绑定电脑域名',
+          `mobile_domain` varchar(50) DEFAULT NULL COMMENT '绑定手机域名',
+          `thumb` varchar(255) NOT NULL COMMENT '栏目图片',
+          `show` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否显示',
+          `content` mediumtext NOT NULL COMMENT '单页内容',
+          `setting` text NOT NULL COMMENT '属性配置',
+          `displayorder` smallint(5) NOT NULL DEFAULT '0',
+          PRIMARY KEY (`id`),
+          KEY `mid` (`mid`),
+          KEY `tid` (`tid`),
+          KEY `show` (`show`),
+          KEY `dirname` (`dirname`),
+          KEY `module` (`pid`,`displayorder`,`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='共享模块栏目表';
+        "));
+        }
+
+        $this->db->simpleQuery("DROP TABLE IF EXISTS `".$this->dbprefix($siteid.'_share_index')."`");
+        $this->db->simpleQuery(dr_format_create_sql("
+        CREATE TABLE IF NOT EXISTS `".$this->dbprefix($siteid.'_share_index')."` (
+          `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+          `mid` varchar(20) NOT NULL COMMENT '模块目录',
+          PRIMARY KEY (`id`),
+          KEY `mid` (`mid`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='共享模块内容索引表';
+        "));
+
+        // 执行应用插件的站点sql语句
+        $local = \Phpcmf\Service::Apps();
+        foreach ($local as $dir => $path) {
+            if (is_file($path.'install.lock') && is_file($path.'Config/Install_site.php')) {
+                $sql = file_get_contents($path.'Config/Install_site.sql');
+                $this->_query(str_replace('{dbprefix}',  $this->preifx.$siteid.'_', $sql));
+            }
+        }
+
+    }
     
 }
