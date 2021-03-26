9271cf310d9a1420052a866214f0136b169cc72b
PHP码农 <dayrui@gmail.com>
2021-03-26 12:52:11 +0800

    完善注释

--- dayrui/Fcms/Http/Controllers/Admin/Seo_site.php
+++ dayrui/Fcms/Http/Controllers/Admin/Seo_site.php
@@ -1,8 +1,8 @@
 <?php namespace Phpcmf\Controllers\Admin;
-
 /**
  * http://www.xunruicms.com
  * 本文件是框架系统文件，二次开发时不可以修改本文件
+ * 迅睿内容管理框架系统（简称：迅睿CMS）软件著作权登记号：2019SR0854684
  **/
 
 
--- dayrui/Fcms/Http/Controllers/Api/Oauth.php
+++ dayrui/Fcms/Http/Controllers/Api/Oauth.php
@@ -1,11 +1,9 @@
 <?php namespace Phpcmf\Controllers\Api;
-use function Composer\Autoload\includeFile;
-
 /**
  * http://www.xunruicms.com
  * 本文件是框架系统文件，二次开发时不可以修改本文件
+ * 迅睿内容管理框架系统（简称：迅睿CMS）软件著作权登记号：2019SR0854684
  **/
-
 
 // 快捷登录接口
 class Oauth extends \Phpcmf\Common
