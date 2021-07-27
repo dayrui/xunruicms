214eb1da98be3590cc8e905013f9b0694d986105
dayrui <dayrui@gmail.com>
2021-07-27 20:12:57 +0800

    支付回调显示地址

--- api/pay/finecms/pay.php
+++ api/pay/finecms/pay.php
@@ -6,15 +6,25 @@
 
 // 判断用户权限
 if (!$this->uid) {
-    $return = dr_return_data(0, dr_lang('你还没有登录'), ['url' => \Phpcmf\Service::L('router')->member_url('login/index')]);
+    $return = dr_return_data(0, dr_lang('你还没有登录'), [
+        'url' => dr_member_url('login/index')
+    ]);
 } elseif ($data['type'] == 'recharge') {
-    $return = dr_return_data(0, dr_lang('充值不能使用余额支付'));
+    $return = dr_return_data(0, dr_lang('充值不能使用余额支付'), [
+        'url' => dr_member_url('paylog/index')
+    ]);
 } elseif ($data['uid'] != $this->uid) {
-    $return = dr_return_data(0, dr_lang('无权限操作'));
+    $return = dr_return_data(0, dr_lang('无权限操作'), [
+        'url' => dr_member_url('paylog/index')
+    ]);
 } elseif ((float)\Phpcmf\Service::C()->member['money'] <= 0 ) {
-    $return = dr_return_data(0, dr_lang('账户余额不足'));
+    $return = dr_return_data(0, dr_lang('账户余额不足'), [
+        'url' => dr_member_url('paylog/index')
+    ]);
 } elseif ((float)\Phpcmf\Service::C()->member['money'] - $data['value'] < 0) {
-    $return = dr_return_data(0, dr_lang('账户可用余额不足'));
+    $return = dr_return_data(0, dr_lang('账户可用余额不足'), [
+        'url' => dr_member_url('paylog/index')
+    ]);
 } else {
     $rt = $this->paysuccess('fc-'.$id, '');
     if (!$rt['code']) {
