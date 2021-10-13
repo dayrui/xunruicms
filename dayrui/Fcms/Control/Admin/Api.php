7db96f83d6c917bcd57a21f43fe0695fb0ec4fd6
dayrui <dayrui@gmail.com>
2021-10-13 12:46:53 +0800

    remove

--- dayrui/Fcms/Control/Admin/Api.php
+++ dayrui/Fcms/Control/Admin/Api.php
@@ -77,26 +77,6 @@ class Api extends \Phpcmf\Common {
         $this->_json(1, dr_lang('操作成功'));
     }
 
-    // 查看流程
-    public function verify() {
-
-        $id = intval(\Phpcmf\Service::L('input')->get('id'));
-        if (!$id) {
-            $this->_json(0, dr_lang('审核流程id不存在'));
-        }
-
-        $data = \Phpcmf\Service::M()->db->table('admin_verify')->where('id', $id)->get()->getRowArray();
-        if (!$data) {
-            $this->_json(0, dr_lang('数据#%s不存在', $id));
-        }
-
-        \Phpcmf\Service::V()->assign([
-            'role' => \Phpcmf\Service::M('Auth')->get_role_all(),
-            'value' => dr_string2array($data['verify']),
-        ]);
-        \Phpcmf\Service::V()->display('verify_show.html');exit;
-    }
-
     // 来自快捷登录
     public function oauth() {
 
