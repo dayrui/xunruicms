70ce09b8d877012a4d18239ebb6ab410a0d03538
dayrui <dayrui@gmail.com>
2021-11-13 01:05:15 +0800

    加入回到顶部按钮

--- static/assets/global/scripts/app.js
+++ static/assets/global/scripts/app.js
@@ -20,6 +20,35 @@ var App = function() {
 
     };
 
+    var handleGoTop = function () {
+        var offset = 100;
+        var duration = 500;
+
+        if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {  // ios supported
+            $(window).bind("touchend touchcancel touchleave", function(e){
+                if ($(this).scrollTop() > offset) {
+                    $('.scroll-to-top').fadeIn(duration);
+                } else {
+                    $('.scroll-to-top').fadeOut(duration);
+                }
+            });
+        } else {  // general
+            $(window).scroll(function() {
+                if ($(this).scrollTop() > offset) {
+                    $('.scroll-to-top').fadeIn(duration);
+                } else {
+                    $('.scroll-to-top').fadeOut(duration);
+                }
+            });
+        }
+
+        $('.scroll-to-top').click(function(e) {
+            e.preventDefault();
+            $('html, body').animate({scrollTop: 0}, duration);
+            return false;
+        });
+    };
+
     // 运行由设置的回调函数 App.addResponsiveHandler().
     var _runResizeHandlers = function() {
         // 初始化其他订阅元素
@@ -318,6 +347,7 @@ var App = function() {
             //Core handlersww
             handleInit(); // initialize core variables
             handleOnResize(); // set and handle responsive
+            handleGoTop(); // set and handle responsive
 
             //UI Component handlerss
             handleMaterialDesign(); //复选框选择控制w
