e3a68f107f78ea1cefb0e128e122ce0207ec6ff3
dayrui <dayrui@gmail.com>
2021-11-22 12:35:25 +0800

    增加后台界面缓存

--- dayrui/Fcms/Init.php
+++ dayrui/Fcms/Init.php
@@ -108,6 +108,7 @@ foreach ($system as $var => $value) {
 }
 unset($my, $system);
 
+!defined('IS_ADMIN_CACHE') && define('IS_ADMIN_CACHE', '1');
 !defined('SQGS') && define('SQGS', '迅睿CMS开源框架');
 !defined('CI_DEBUG') && define('CI_DEBUG', IS_DEV ? 1 : IS_ADMIN && SYS_DEBUG);
 !defined('IS_FB_DEBUG') && define('IS_FB_DEBUG', CI_DEBUG ? 0 : 1);
