48015c982deb08877eca4f2b9ab886f5007f2fbd
dayrui <dayrui@gmail.com>
2022-03-24 21:02:49 +0800

    version install

--- dayrui/My/Config/Install.php
+++ dayrui/My/Config/Install.php
@@ -8,10 +8,7 @@ if (isset($_GET['is_install_db']) && $_GET['is_install_db']) {
     if (is_file(APPSPATH.'Module/Config/App.php')) {
         $rt = \Phpcmf\Service::M('app')->install('module');
         if ($rt['code']) {
-            if (!defined('IS_USE_MODULE')) {
-                define('IS_USE_MODULE', 1);
-            }
-            \Phpcmf\Service::M('module')->install('news', null, 0, 1);
+            \Phpcmf\Service::M('module', 'module')->install('news', null, 0, 1);
             $sql = file_get_contents(MYPATH.'Config/demo.sql');
             if ($sql) {
                 $sql = str_replace('{dbprefix}', \Phpcmf\Service::M()->prefix, $sql);
