6304b2d343e3670101f817504d73be21384e39f1
dayrui <dayrui@gmail.com>
2022-05-24 12:11:55 +0800

    no commit message

--- dayrui/Fcms/Field/Field.php
+++ dayrui/Fcms/Field/Field.php
@@ -96,15 +96,6 @@ return [
         'name' => '属性参数',
     ],
     [
-        'id' => 'Redirect',
-        'name' => '转向链接',
-        'used' => ['module'],
-    ],
-    [
-        'id' => 'Related',
-        'name' => '内容关联',
-    ],
-    [
         'id' => 'Uid',
         'name' => '账号',
     ],
@@ -136,17 +127,6 @@ return [
         'id' => 'Ftable',
         'name' => '填写表格',
     ],
-    [
-        'id' => 'Catids',
-        'name' => '副栏目',
-        'used' => ['module'],
-    ],
-    [
-        'id' => 'Cat',
-        'name' => '模块栏目（单选）',
-    ],
-    [
-        'id' => 'Cats',
-        'name' => '模块栏目（多选）',
-    ],
+
+
 ];
