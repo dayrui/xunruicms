dba17c795e0aea08870ffa2e050a23586d89ba73
dayrui <dayrui@gmail.com>
2022-05-26 00:07:35 +0800

    Laravel记录sql变量

--- dayrui/Laravel/System/app/Listeners/QueryListener.php
+++ dayrui/Laravel/System/app/Listeners/QueryListener.php
@@ -9,9 +9,8 @@ class QueryListener
     // 记录查询的sql
     public function handle(QueryExecuted $event)
     {
-        $sql = str_replace("?", "'%s'", $event->sql);
-        $sql = vsprintf($sql, $event->bindings);
-
-        \Phpcmf\Service::M()->db->setLastQuery($sql);
+        $sql = str_replace("?", "'%s'", str_replace('%', '_XS_', $event->sql));
+        $event->bindings && $sql = vsprintf($sql, $event->bindings);
+        \Phpcmf\Service::M()->db->setLastQuery(str_replace('_XS_', '%', $sql));
     }
 }
