7ba6e597bae39adc6be19ad4f62c2670d0981c2f
dayrui <dayrui@gmail.com>
2022-05-31 22:46:38 +0800

    系统更新中增加更新联动菜单选项

--- dayrui/ThinkPHP/Extend/Run.php
+++ dayrui/ThinkPHP/Extend/Run.php
@@ -5,7 +5,7 @@ namespace think;
 require FRAMEPATH . 'System/vendor/autoload.php';
 
 // 执行HTTP应用并响应
-$http = (new App())->debug(true)->http;
+$http = (new App())->debug(CI_DEBUG ? true : false)->http;
 
 $response = $http->run();
 
