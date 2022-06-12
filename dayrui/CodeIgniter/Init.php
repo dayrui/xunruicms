cf60f85640292b83f52e85a13cae5075bff70269
dayrui <dayrui@gmail.com>
2022-06-12 14:49:00 +0800

    最低支持php71

--- dayrui/CodeIgniter/Init.php
+++ dayrui/CodeIgniter/Init.php
@@ -13,7 +13,7 @@ define('SYSTEMPATH', BASEPATH);
 
 define('FRAME_PHP_VERSION', '7.4');  // PHP最低版本
 if (version_compare(PHP_VERSION, FRAME_PHP_VERSION) < 0) {
-    exit("<font color=red>PHP版本要求在".FRAME_PHP_VERSION."及以上，当前".PHP_VERSION."</font><hr>最低支持PHP7.2环境，需要在这里下载兼容包：https://www.xunruicms.com/doc/1166.html");
+    exit("<font color=red>PHP版本要求在".FRAME_PHP_VERSION."及以上，当前".PHP_VERSION."</font><hr>最低支持PHP7.1环境，需要在这里下载兼容包：https://www.xunruicms.com/doc/1166.html");
 }
 
 /*
