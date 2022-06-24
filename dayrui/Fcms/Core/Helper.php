fbc1ffabcb6fe5f8b52d1a690e62ba99992ef8f9
dayrui <dayrui@gmail.com>
2022-06-24 14:31:37 +0800

    api请求下可设置相对路径

--- dayrui/Fcms/Core/Helper.php
+++ dayrui/Fcms/Core/Helper.php
@@ -3418,7 +3418,7 @@ function dr_web_prefix($url) {
  */
 function dr_url_rel($url) {
 
-    if ((IS_API && !SYS_API_REL) || IS_ADMIN) {
+    if ((IS_API_HTTP && (!defined('SYS_API_REL') || !SYS_API_REL)) || IS_ADMIN) {
         return $url;
     } elseif (defined('SYS_URL_REL') && SYS_URL_REL) {
         $surl = FC_NOW_HOST;
@@ -3443,7 +3443,7 @@ function dr_url_rel($url) {
  */
 function dr_text_rel($text) {
 
-    if ((IS_API && !SYS_API_REL) || IS_ADMIN) {
+    if ((IS_API_HTTP && (!defined('SYS_API_REL') || !SYS_API_REL)) || IS_ADMIN) {
         return $text;
     } elseif (defined('SYS_URL_REL') && SYS_URL_REL) {
         $surl = FC_NOW_HOST;
