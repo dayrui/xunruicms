1c0c94f58e753aa57d366ed088d610598ff630cc
dayrui <dayrui@qq.com>
2022-11-02 11:05:04 +0800

    禁止文件上传配置文件示范

---
+++ config/fileext.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// 禁止以下文件上传
+$this->notallowed = [
+    'php',
+    'php3',
+    'asp',
+    'jsp',
+    'jspx',
+    'aspx',
+    'exe',
+    'sh',
+    'phtml',
+];
+
+// 允许远程下载文件扩展名
+$this->down_file_ext = [
+    'jpg',
+    'jpeg',
+    'gif',
+    'png',
+    'webp',
+    'zip',
+    'aaa',
+];
