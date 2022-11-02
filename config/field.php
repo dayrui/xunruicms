a04c242a0aa64ed32af727987f23f968266fb519
dayrui <dayrui@qq.com>
2022-11-02 11:03:10 +0800

    默认字段样式示范代码

--- config/field.php
+++ config/field.php
@@ -5,7 +5,24 @@
  */
 
 return [
-    
+
+    // 后台
+    'admin' => '<div class="form-group" rs="is_admin" id="dr_row_{name}">
+    <label class="control-label col-md-2">{text}</label>
+    <div class="col-md-10">{value}</div>
+</div>',
+
+    // 用户中心
+    'member' => '<div class="form-group" rs="is_member" id="dr_row_{name}">
+    <label class="control-label col-md-2">{text}</label>
+    <div class="col-md-10">{value}</div>
+</div>',
+
+    // 前台
+    'home' => '<div class="form-group" rs="is_home" id="dr_row_{name}">
+    <label class="control-label col-md-2">{text}</label>
+    <div class="col-md-10">{value}</div>
+</div>',
     
     
 ];
