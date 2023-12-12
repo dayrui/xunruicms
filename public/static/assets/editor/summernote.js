24dcff0eb15bdb6b473c358127eba6f907e3b1b9
dayrui <dayrui@gmail.com>
2023-12-12 10:14:32 +0800

    Editor简化编辑器，增加拖动图片上传功能

--- public/static/assets/editor/summernote.js
+++ public/static/assets/editor/summernote.js
@@ -6318,26 +6318,24 @@ var Clipboard_Clipboard = /*#__PURE__*/function () {
     key: "pasteByEvent",
     value: function pasteByEvent(event) {
       var _this = this;
-
       var clipboardData = event.originalEvent.clipboardData;
-
       if (clipboardData && clipboardData.items && clipboardData.items.length) {
-        var item = clipboardData.items.length > 1 ? clipboardData.items[1] : lists.head(clipboardData.items);
-
-        if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
-          // paste img file
-          this.context.invoke('editor.insertImagesOrCallback', [item.getAsFile()]);
-          event.preventDefault();
-        } else if (item.kind === 'string') {
-          // paste text with maxTextLength check
-          if (this.context.invoke('editor.isLimited', clipboardData.getData('Text').length)) {
+        for (var i = 0, len = clipboardData.items.length; i < len; i++) {
+          var item = clipboardData.items[i];
+          if (item.kind === 'file' && item.type.indexOf('image/') !== -1) {
+            // paste img file
+            this.context.invoke('editor.insertImagesOrCallback', [item.getAsFile()]);
             event.preventDefault();
+          } else if (item.kind === 'string') {
+            // paste text with maxTextLength check
+            if (this.context.invoke('editor.isLimited', clipboardData.getData('Text').length)) {
+              event.preventDefault();
+            }
           }
         }
       } else if (window.clipboardData) {
         // for IE
         var text = window.clipboardData.getData('text');
-
         if (this.context.invoke('editor.isLimited', text.length)) {
           event.preventDefault();
         }
