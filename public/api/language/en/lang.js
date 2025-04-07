849f8ed75ecd688af1963a4c4d12287b123c3431
dayrui <dayrui@gmail.com>
2025-04-08 00:01:55 +0800

    增加en语言包

---
+++ public/api/language/en/lang.js
@@ -0,0 +1,137 @@
+// js语言包
+var lang = new Array;
+lang['ok'] = 'OK';
+lang['ts'] = 'Prompt';
+lang['ip'] = 'ip information';
+lang['esc'] = 'Esc';
+lang['add'] = 'Add';
+lang['添加'] = 'Add';
+lang['查看'] = 'Show';
+lang['edit'] = 'Change';
+lang['修改'] = 'Change';
+lang['系统错误'] = 'Error';
+lang['错误日志'] = 'Error Log';
+lang['查看日志'] = 'Show Log';
+lang['用户注册协议'] = 'User Registration Agreement';
+lang['系统崩溃，请检查错误日志'] = 'System Error';
+lang['表单id属性已重复定义'] = 'The form ID attribute has been defined repeatedly';
+lang['code'] = 'Code';
+lang['show'] = 'Show';
+lang['send'] = 'Send';
+lang['推送'] = 'Send';
+lang['保存'] = 'Save';
+lang['save'] = 'Save';
+lang['copy'] = 'Copy';
+lang['error'] = 'Error';
+lang['error_admin'] = 'System crash, please check the error log';
+lang['member'] = 'user information';
+lang['paylog'] = 'transaction record';
+lang['todoing'] = 'results of execution';
+lang['logout'] = 'Are you sure you want to exit？';
+lang['htmlfile'] = 'Generate static page';
+lang['protocol'] = 'User Registration Agreement';
+lang['unloadtips'] = 'Data not saved, are you sure you want to leave？';
+lang['unformid'] = 'The form ID attribute does not exist';
+lang['repeatformid'] = 'The form ID attribute has been defined repeatedly';
+lang['复制'] = 'Copy';
+lang['打印'] = 'print';
+lang['关闭后将中断操作，是否确认关闭呢？'] = 'After closing, the operation will be interrupted. Are you sure you want to close it?';
+lang['正在努力地加载数据中，请稍候'] = 'Trying to load data, please wait';
+lang['每页显示'] = 'Display per page ';
+lang['条记录'] = ' record';
+lang['显示第'] = 'Display the ';
+lang['到第'] = ' To ';
+lang['条记录，总共'] = ' Records, total ';
+lang['条记录（从'] = ' Records (filtered from ';
+lang['总记录中过滤）'] = ' total records)';
+lang['上一页'] = 'previous page';
+lang['下一页'] = 'next page';
+lang['第'] = 'the';
+lang['页'] = 'page';
+lang['总共'] = 'total';
+lang['清空过滤'] = 'Clear filter';
+lang['搜索'] = 'Search';
+lang['没有数据'] = 'No Data';
+lang['隐藏/显示分页'] = 'Hide/Show Paging';
+lang['显示分页'] = 'display paging';
+lang['隐藏分页'] = 'Hide pagination';
+lang['刷新'] = 'Refresh';
+lang['卡片开启'] = 'Card activation';
+lang['卡片隐藏'] = 'Card hiding';
+lang['卡片开启'] = 'Card hiding';
+lang['列'] = 'Rows';
+lang['切换所有'] = 'Switch all';
+lang['所有'] = 'All';
+lang['自动刷新'] = 'auto refresh';
+lang['导出数据'] = 'Export Data';
+lang['跳转'] = 'Jump To';
+lang['高级搜索'] = 'advanced search';
+lang['关闭'] = 'Close';
+lang['隐藏/显示过滤控制'] = 'Hide/Show Filter Control';
+lang['隐藏过滤控制'] = 'Hide Filter Control';
+lang['显示过滤控制'] = 'Show Filter Control';
+lang['没有选择数据项'] = 'No data item selected';
+lang['提示'] = 'Prompt';
+lang['确定'] = 'Ok';
+lang['取消'] = 'Esc';
+lang['独立'] = 'Independent';
+lang['你确定要安装到当前站点吗？'] = 'Are you sure you want to install to the current site?';
+lang['共享'] = 'Shared';
+lang['安装选择'] = 'Installation Selection';
+lang['请关闭当前窗口'] = 'Please close the current window';
+lang['确定要刷新整个后台吗？'] = 'Are you sure you want to refresh the entire backend?';
+lang['确定要刷新整个后台吗？'] = 'Are you sure you want to refresh the entire backend?';
+lang['刷新后台'] = 'Refresh the backend';
+lang['直接进入'] = 'Direct Entry';
+lang['共享模块: 共用一个栏目，在栏目中选择模块'] = 'Shared module: Share a column and select a module in the column';
+lang['独立模块: 独立栏目管理，在模块中选择栏目'] = 'Independent module: Independent column management, select a column in the module';
+lang['通用标签'] = 'Generalized label';
+lang['SEO连接符号，默认是_'] = 'SEO connection symbol, default is _';
+lang['分页页码'] = 'Page numbers for pagination';
+lang['网站名称'] = 'Site Name';
+lang['支持表字段，格式：{字段名}'] = 'Support table fields, format: {field name}';
+lang['例如：{title}表示标题'] = 'For example, {title} represents the title';
+lang['支持网站系统常量，格式：{大写的常量名称}'] = 'Support website system constants, format: {capitalized constant name}';
+lang['例如：{SITE_NAME}表示网站名称'] = 'For example, {SITE_NAME} represents the website name';
+lang['举例'] = 'give an example';
+lang['默认地址'] = 'Default Address';
+lang['通配符'] = 'Wildcard';
+lang['表示当前模块目录'] = 'Indicate the current module directory';
+lang['表示id'] = 'Indicate ID';
+lang['表示年'] = 'Indicate year';
+lang['表示月'] = 'Expressing months';
+lang['表示日'] = 'Express day';
+lang['表示分页号'] = 'Indicate page number';
+lang['表示栏目目录名称'] = 'Indicate the name of the column directory';
+lang['包含父级子级层次的目录'] = 'A directory containing parent child hierarchies';
+lang['表示父级目录名称'] = 'Indicate the name of the parent directory';
+lang['表示顶级目录名称'] = 'Indicate the top-level directory name';
+lang['表示模块目录'] = 'Indicate module directory';
+lang['表示搜索参数'] = 'Representing search parameters';
+lang['请联系开发商'] = 'Please contact the developer';
+lang['不支持伪静态，首先需要确定服务器支持rewrite模块并开启了，其次需要正确配置上面的方法'] = 'Not Supported';
+lang['表单id属性不存在'] = 'The form ID attribute does not exist';
+lang['退出成功'] = 'Exit successful';
+lang['有必填字段未填写，确认提交吗？'] = 'Are you sure to submit the required fields that have not been filled in?';
+lang['交易记录'] = 'transaction record';
+lang['代码'] = 'Code';
+lang['aaaaaa'] = 'aaaaa';
+lang['aaaaaa'] = 'aaaaa';
+lang['aaaaaa'] = 'aaaaa';
+lang['aaaaaa'] = 'aaaaa';
+lang['aaaaaa'] = 'aaaaa';
+
+
+// 日期类型语言包
+var finecms_datepicker_lang = {
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    daysShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    daysMin:  ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    monthsShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    today: "Today",
+    clear: "Clear",
+    suffix: [],
+    meridiem: [],
+    titleFormat: "MM yyyy"
+};
